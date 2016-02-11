138e69bf75a0835f77a89e1c65e21ed80e6df164
Brian Keating <b@briankeating.net>
2016-02-11 14:52:28 +0530

    working poc

--- app/app.ts
+++ app/app.ts
@@ -20,39 +20,4 @@ module test{
     app.controller('MainCtrl', MainCtrl);
 
 
-    function widgetTest(){
-        return {
-            templateUrl: 'widget.tmpl.html',
-            link : function(scope, element, attrs){
-                var widget = element.find('.widgetContent');
-
-                widget.draggable();
-                widget.resizable();
-
-                element.on('resize', function(){
-                    var wcaHeight = $('.widgetContentArea').height();
-                    var noteTbHeight = $('.note-toolbar').height();
-                    var diff = wcaHeight-noteTbHeight - 20; // 20 padding
-
-                    $('div.note-editable').height(diff);
-                });
-
-
-            }
-        }
-
-    }
-
-    app.directive('widgetTest', [widgetTest]);
-
-    //jQuery crap
-
-    declare var $ : any;
-
-
-
-
-
-
-
 }
---
+++ app/directives.ts
@@ -0,0 +1,44 @@
+/// <reference path="..\typings\main.d.ts" />
+
+
+var app = angular.module('myApp');
+
+
+
+function widgetTest(){
+    return {
+        templateUrl: 'app/widget.tmpl.html',
+        link : function(scope, element, attrs){
+
+            function setHeight(){
+                var wcaHeight = $('.widgetContentArea').height();
+                var noteTbHeight = $('.note-toolbar').height();
+                var diff = wcaHeight-noteTbHeight - 20; // 20 padding
+
+                diff = diff - 30; // bottom spacing for status bar
+                $('div.note-editable').height(diff);
+            }
+
+
+            var widget = element.find('.widget');
+
+            widget.draggable({
+                cursor: "move",
+                handle: '.header',
+                cancel:'.widgetContent'
+            });
+
+            widget.resizable();
+
+            element.on('resize', function(){
+                setHeight();
+            });
+
+            setHeight();
+
+        }
+    }
+
+
+}
+app.directive('widgetTest', [widgetTest]);
